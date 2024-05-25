3da099e9753b520e2e00444fc04a8345a1dd1306
Iyimide Adekile <adekileiyimide@gmail.com>
2024-05-25 17:29:30 +0100

    Added suport for param routes + can access params from event.params + not found handler / New API next - "Wildcard routes" / No tests written yet / Manual test in run.dart

--- bin/run.dart
+++ bin/run.dart
@@ -1,6 +1,6 @@
 // import 'package:console/console.dart' as console;
 
-import 'package:h3/http-layer/H4.dart';
+import 'package:h3/http-layer/h4.dart';
 import 'package:h3/http-layer/index.dart';
 
 void main(List<String> arguments) async {
@@ -10,41 +10,8 @@ void main(List<String> arguments) async {
   app.use(router);
 
   router.post("/", (event) => "HIII");
-}
-
-class Person {
-  String? name;
-}
-
-class Stack<E> {
-  Stack() : _storage = <E>[];
-  final List<E> _storage;
-  var top = 0;
-
-  @override
-  String toString() {
-    return '--- Top ---\n'
-        '${_storage.reversed.join('\n')}'
-        '\n-----------';
-  }
-
-  void printType() {
-    print(_storage.runtimeType);
-  }
-
-  void push(E element) {
-    _storage.add(element);
-  }
-
-  void pop() {
-    _storage.removeLast();
-  }
-}
-
-StringBuffer dartIsFun() {
-  var message = StringBuffer('Dart is fun');
-  for (var i = 0; i < 5; i++) {
-    message.write('!');
-  }
-  return message;
+  router.get("/:id", (event) {
+    print(event.params);
+    return "Hi ${event.params?["ïd"]}";
+  });
 }
