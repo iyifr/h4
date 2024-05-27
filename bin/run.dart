ae1e08f665c446304bc222aaedf11f64f675ed51
Iyimide Adekile <adekileiyimide@gmail.com>
2024-05-27 20:39:22 +0100

    Added support for wildcard routes

--- bin/run.dart
+++ bin/run.dart
@@ -7,7 +7,10 @@ void main(List<String> arguments) async {
   var router = createRouter();
 
   app.use(router);
-  router.get("/", (event) => "<p>Hello world my name is destroyer</p>");
-  router.get(
-      "/:id/hi/bye/shy", (event) => "<p>Hiiiiii ${event.params?["id"]}</p>");
+  // router.get("/", (event) => "<p>Hello world my name is destroyer</p>");
+  // router.get(
+  //     "/:id/hi/bye/shy", (event) => "<p>Hiiiiii ${event.params?["id"]}</p>");
+
+  router.get("/*", (event) => "welcome");
+  router.get("/25/**", (event) => "welcome hooooommeee");
 }
