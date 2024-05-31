d49c491ddfa65d8efa683f19081e59f1479f6fff
Iyimide Adekile <adekileiyimide@gmail.com>
2024-05-31 15:52:28 +0100

    Lots of changes in response to feedback from dart discord

--- bin/run.dart
+++ bin/run.dart
@@ -1,30 +1,31 @@
-// // import 'package:console/console.dart' as console;
+// import 'package:console/console.dart' as console;
 
-// import 'package:h4/create.dart';
-// import 'package:h4/src/create_error.dart';
+import 'package:h4/create.dart';
+import 'package:h4/src/create_error.dart';
 
-// void main(List<String> arguments) async {
-//   // var app = createApp();
-//   // var router = createRouter();
+void main(List<String> arguments) async {
+  var app = createApp();
+  print("App is running on ${app.port}");
+  var router = createRouter();
 
-//   // app.use(router);
+  app.use(router);
 
-//   // app.onRequest((event) {
-//   //   print(event.path);
-//   // });
+  app.onRequest((event) {
+    print(event.path);
+  });
 
-//   // app.onError((e, s) {
-//   //   print("$e");
-//   // });
+  app.onError((e, s) {
+    print("$e");
+  });
 
-//   // router.get("/25/**", (event) => throw Exception("WtF"));
+  router.get<bool>("/25/**", (event) => true);
 
-//   // router.post("/vamos", (event) async {
-//   //   await Future.delayed(Duration(milliseconds: 200));
-//   //   return "HELLLO MANYANA";
-//   // });
+  router.post("/vamos", (event) async {
+    await Future.delayed(Duration(milliseconds: 200));
+    return "HELLLO MANYANA";
+  });
 
-//   // router.get("/vamos", (event) {
-//   //   throw CreateError('Lol nahhh');
-//   // });
-// }
+  router.get("/vamos", (event) {
+    throw CreateError(message: 'Error occured', errorCode: 400);
+  });
+}
